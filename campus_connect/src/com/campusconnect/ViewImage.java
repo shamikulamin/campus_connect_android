7e06e6d91c6fb95c1adbd89c4d20f00a78ce4f91
shamikulamin <shamikulamin@2b99b6ed-cb11-4472-9ac8-5cc0b8f6dc15>
2013-06-12 21:49:17 +0000

    Class for viewing images, this uses a more updated code than we previously used.

---
+++ campus_connect/src/com/campusconnect/ViewImage.java
@@ -0,0 +1,75 @@
+package com.campusconnect;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.media.MediaScannerConnection.MediaScannerConnectionClient;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+public class ViewImage implements MediaScannerConnectionClient{
+    public String[] allFiles;
+    private Context ctx;
+    private String SCAN_PATH ;
+    private static final String FILE_TYPE = "image/png";
+
+    private MediaScannerConnection conn;
+    /** Called when the activity is first created. */
+   // @Override
+    public ViewImage(Context ctx) {
+    	this.ctx=ctx;
+    	//super.onCreate(savedInstanceState);
+    	//setContentView(R.layout.main);
+
+    	File imagesFolder = new File(Environment.getExternalStorageDirectory(), "MyImages");
+    	allFiles = imagesFolder.list();
+ //   uriAllFiles= new Uri[allFiles.length];
+    	for(int i=0;i<allFiles.length;i++)
+    	{
+    		Log.d("all file path"+i, allFiles[i]+allFiles.length);
+    	}
+  //  Uri uri= Uri.fromFile(new File(Environment.getExternalStorageDirectory().toString()+"/yourfoldername/"+allFiles[0]));
+    	SCAN_PATH=Environment.getExternalStorageDirectory().toString()+"/MyImages/"+allFiles[0];
+    	startScan();
+    }
+		
+		
+		@Override
+		public void onMediaScannerConnected() {
+			Log.d("onMediaScannerConnected","success"+conn);
+			conn.scanFile(SCAN_PATH, FILE_TYPE);    
+		}
+		@Override
+		public void onScanCompleted(String path, Uri uri) {
+			try {
+				Log.d("onScanCompleted",uri + "success"+conn);
+				if (uri != null) 
+				{
+					
+					Intent intent = new Intent(Intent.ACTION_VIEW);
+					intent.setData(uri);
+					
+					ctx.startActivity(intent);
+				}
+			} finally 
+			{
+				conn.disconnect();
+				conn = null;
+			}
+		}
+		
+		public void startScan() {
+				Log.d("Connected","success"+conn);
+				if(conn!=null)
+				{
+					conn.disconnect();
+				}
+				conn = new MediaScannerConnection(ctx,this);
+				conn.connect();
+		}
+	}
